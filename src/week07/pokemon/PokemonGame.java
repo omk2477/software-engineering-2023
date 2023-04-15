66c11ffe070b36b9d875f109c5bf9600a79a9eee
omk2477 <omk2477@gmail.com>
2023-04-15 11:17:07 +0900

    factory method로 변경

--- src/week07/pokemon/PokemonGame.java
+++ src/week07/pokemon/PokemonGame.java
@@ -1,20 +1,23 @@
 package week07.pokemon;
 
-class Pokemon {
+interface Battleable {
+    void attack();
+}
+class Pokemon implements Battleable {
     public void attack() {
         System.out.println("공격!");
     }
 }
 
 class Trainer {
-    private final Pokemon pokemon;
+    private final Battleable battleable;
 
-    public Trainer() {
-        pokemon = new Pokemon();
+    public Trainer(Battleable battleable) {
+        this.battleable = battleable;
     }
 
     public void battle() {
-        pokemon.attack();
+        battleable.attack();
     }
 
     public void catchPokemon() {
@@ -24,7 +27,8 @@ class Trainer {
 
 public class PokemonGame {
     public static void main(String[] args) {
-        Trainer jiwoo = new Trainer();
+        Pokemon pokemon = new Pokemon();
+        Trainer jiwoo = new Trainer(pokemon);
         jiwoo.catchPokemon();
         jiwoo.battle();
     }
